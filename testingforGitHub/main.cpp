aeba046360c315e6b9b2f244e7b4c316c5f5dd7f
zuri9304 <zuri9304@pacificu.edu>
2023-02-14 20:56:42 -0800

    Initial Commit

---
+++ testingforGitHub/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  testingforGitHub
+//
+//  Created by Gabriel Rivera on 2/14/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
